fa3f84b0a31a8e9b9b47339d5432c28ff992eb48
Tomasvii <tomasvillarrealfederco@gmail.com>
2023-11-09 17:29:07 -0300

    form responsive fix

--- public/js/total.js
+++ public/js/total.js
@@ -4,6 +4,12 @@ const totalDisplay2 = document.getElementById("total2");
 const quantity = document.getElementById("cantidad");
 const priceUnit = document.getElementById("price");
 
+const unitInput2 = document.getElementById("cantidad-input2");
+const totalDisplay21 = document.getElementById("total21");
+const totalDisplay22 = document.getElementById("total22");
+const quantity2 = document.getElementById("cantidad2");
+const priceUnit2 = document.getElementById("price2");
+
 unitInput.addEventListener("input", function () {
     const unitValue = parseFloat(unitInput.value);
     const pricePerUnit = parseFloat(priceUnit.innerText);
@@ -16,5 +22,20 @@ unitInput.addEventListener("input", function () {
         quantity.innerHTML = `<strong>--</strong>`;
         totalDisplay.innerHTML = `--`;
         totalDisplay2.innerHTML = `Total: <strong>$ --</strong>`;
+    }
+});
+
+unitInput2.addEventListener("input", function () {
+    const unitValue2 = parseFloat(unitInput2.value);
+    const pricePerUnit2 = parseFloat(priceUnit2.innerText);
+    const total2 = unitValue2 * pricePerUnit2;
+    if (!isNaN(total2)) {
+        quantity2.innerHTML = `<strong>${unitValue2.toLocaleString()}</strong>`;
+        totalDisplay21.innerHTML = `<strong>$ ${total2.toLocaleString()}</strong>`;
+        totalDisplay22.innerHTML = `Total: <strong>$ ${total2.toLocaleString()}</strong>`;
+    } else {
+        quantity2.innerHTML = `<strong>--</strong>`;
+        totalDisplay21.innerHTML = `--`;
+        totalDisplay22.innerHTML = `Total: <strong>$ --</strong>`;
     }
 });
