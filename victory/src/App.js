2c25cee7d015f12dac64169a5dcfeaf23c43397f
Tomasvii <tomasvillarrealfederco@gmail.com>
2024-05-09 12:51:16 -0300

    react in progress

---
+++ victory/src/App.js
@@ -0,0 +1,13 @@
+import Nav from "./components/nav";
+import "./assets/css/bootstrap.css";
+import "./assets/css/styles.css";
+
+function App() {
+    return (
+        <div>
+            <Nav />
+        </div>
+    );
+}
+
+export default App;
